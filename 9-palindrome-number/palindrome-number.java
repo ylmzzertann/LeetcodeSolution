3ec906674940e3082d2f0e265ba186329fef708a
ylmzzertann <ertanylmz2012@hotmail.com>
2025-04-07 23:53:19 -0400

    Time: 5 ms (71.96%) | Memory: 43.7 MB (97.84%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x, reversed = 0;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
