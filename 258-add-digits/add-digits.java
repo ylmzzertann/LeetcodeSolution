3d8688bad55bf39dd868f25317416c2fb6653589
ylmzzertann <ertanylmz2012@hotmail.com>
2025-04-19 23:54:05 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (13.26%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,13 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;
+        return 1 + (num - 1) % 9;
     }
 }
