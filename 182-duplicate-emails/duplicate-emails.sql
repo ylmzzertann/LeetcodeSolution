0cee3afa2fa04377e09d85c606f92d567fad1f1e
ylmzzertann <ertanylmz2012@hotmail.com>
2025-04-28 23:54:18 -0400

    Time: 371 ms (81.91%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT DISTINCT p1.email
-FROM Person p1
-JOIN Person p2 ON p1.email = p2.email AND p1.id <> p2.id;
+SELECT email FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
