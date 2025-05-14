90e255814ad7dae5e082e728ae4081cfa198a3e5
ylmzzertann <ertanylmz2012@hotmail.com>
2025-05-13 23:49:31 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (31.85%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -6,13 +6,13 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             
-            // Devamsızlık kontrolü
+            
             if (c == 'A') {
                 absents++;
                 if (absents > 1) return false;
             }
 
-            // Geç kalma kontrolü
+         
             if (c == 'L') {
                 lateStreak++;
                 if (lateStreak >= 3) return false;
