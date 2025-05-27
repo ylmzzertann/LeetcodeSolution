aba247b9c2286c188d31aaac0114a048ebfb71af
ylmzzertann <ertanylmz2012@hotmail.com>
2025-05-26 23:58:21 -0400

    Time: 528 ms (76.55%) | Memory: 0B (100.00%) - LeetSync

--- 1837-daily-leads-and-partners/daily-leads-and-partners.sql
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -1,11 +1,18 @@
 # Write your MySQL query statement below
-SELECT 
-    date_id, 
-    make_name, 
-    COUNT(DISTINCT lead_id) AS unique_leads, 
-    COUNT(DISTINCT partner_id) AS unique_partners
-FROM 
-    DailySales
-GROUP BY 
-    date_id, 
-    make_name;
+SELECT
+    ds.date_id,
+    ds.make_name,
+    ul.unique_leads,
+    up.unique_partners
+FROM
+    (SELECT date_id, make_name, COUNT(*) AS unique_leads
+     FROM (SELECT DISTINCT date_id, make_name, lead_id FROM DailySales) AS leads
+     GROUP BY date_id, make_name) AS ul
+JOIN
+    (SELECT date_id, make_name, COUNT(*) AS unique_partners
+     FROM (SELECT DISTINCT date_id, make_name, partner_id FROM DailySales) AS partners
+     GROUP BY date_id, make_name) AS up
+ON ul.date_id = up.date_id AND ul.make_name = up.make_name
+JOIN
+    (SELECT DISTINCT date_id, make_name FROM DailySales) AS ds
+ON ds.date_id = ul.date_id AND ds.make_name = ul.make_name;
