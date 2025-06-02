faecbd90e37746ce933e11c5266b60aa46c33a9d
ylmzzertann <ertanylmz2012@hotmail.com>
2025-06-01 23:18:59 -0400

    Time: 325 ms (70.02%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -2,6 +2,8 @@
 SELECT user_id, email
 FROM Users
 WHERE 
-    email LIKE '%@%.com'
-    AND email REGEXP '^[a-zA-Z0-9_]+@[a-zA-Z]+\\.com$'
+    email LIKE '%@%.com' -- sadece temel geçerli formatı alanlar
+    AND LENGTH(email) - LENGTH(REPLACE(email, '@', '')) = 1 -- yalnızca 1 tane @ olmalı
+    AND SUBSTRING_INDEX(email, '@', 1) REGEXP '^[a-zA-Z0-9_]+$' -- @ öncesi sadece alfanümerik ve _
+    AND SUBSTRING_INDEX(SUBSTRING_INDEX(email, '.', 1), '@', -1) REGEXP '^[a-zA-Z]+$' -- @ sonrası .com'dan önceki domain sadece harf
 ORDER BY user_id;
