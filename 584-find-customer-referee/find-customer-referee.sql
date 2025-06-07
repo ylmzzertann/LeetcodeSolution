dcaa6bb6a3475e50676c484225875dc15b845103
ylmzzertann <ertanylmz2012@hotmail.com>
2025-06-06 23:56:58 -0400

    Time: 431 ms (98.77%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,14 +1,7 @@
-# Write your MySQL query statement below
 SELECT
   name
 FROM
   Customer
 WHERE
-  id NOT IN (
-    SELECT
-      id
-    FROM
-      Customer
-    WHERE
-      referee_id = 2
-  );
+  referee_id <> 2
+  OR referee_id IS NULL;
