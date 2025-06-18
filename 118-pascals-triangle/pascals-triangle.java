efb5acea92a267fc1e67c74096289d1a7fa022cd
ylmzzertann <ertanylmz2012@hotmail.com>
2025-06-17 22:52:44 -0400

    Time: 1 ms (84.35%) | Memory: 42.3 MB (14.76%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -6,10 +6,9 @@ class Solution {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
-                    row.add(1); // İlk ve son elemanlar 1
+                    row.add(1); 
                 } else {
-                    // Üst satırdan iki elemanın toplamı
-                    row.add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+                        row.add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
                 }
             }
             triangle.add(row);
