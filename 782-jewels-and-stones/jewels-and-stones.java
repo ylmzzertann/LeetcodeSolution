d3a655f60669d0d212454167a5592af443acabaa
ylmzzertann <ertanylmz2012@hotmail.com>
2025-07-01 23:27:02 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (43.14%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char stone : stones.toCharArray()) {
+            if (jewels.indexOf(stone) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
