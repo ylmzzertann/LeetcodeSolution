8983c1bfa77cfdfbb25c83cd9680109c5738245a
ylmzzertann <ertanylmz2012@hotmail.com>
2025-07-06 23:49:54 -0400

    Time: 1 ms (99.87%) | Memory: 44.7 MB (40.56%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        // Adım 1: Sayıların frekansını tutmak için 101 elemanlı bir dizi oluşturulur.
+        // Problem kısıtlamalarına göre sayılar 0 ile 100 arasındadır.
+        int[] counts = new int[101];
+        for (int num : nums) {
+            counts[num]++;
+        }
+
+        // Adım 2: Kümülatif toplam hesaplanır.
+        // Bu adımdan sonra, counts[i] dizisi, i'den küçük sayıların toplam adedini tutar.
+        for (int i = 1; i < 101; i++) {
+            counts[i] += counts[i - 1];
+        }
+
+        // Adım 3: Sonuç dizisi oluşturulur.
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+
+            // Eğer sayı 0 ise, ondan küçük sayı yoktur.
+            if (num == 0) {
+                result[i] = 0;
+            } else {
+                // Bir sayıdan küçük olanların adedi, kümülatif toplam dizisindeki
+                // kendisinden bir önceki indekste saklanan değerdir.
+                result[i] = counts[num - 1];
+            }
+        }
+
+        return result;
+    }
+}
