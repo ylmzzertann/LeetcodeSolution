eb91d5b9c9b42ec69ca54f2431194a504f8db94c
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2025-07-26 23:52:57 -0400

    Time: 1 ms (83.18%) | Memory: 41.6 MB (79.02%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,14 +1,19 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean checkIfPangram(String sentence) {
         if (sentence.length() < 26) return false;
 
-        HashSet<Character> letters = new HashSet<>();
-        for (char c : sentence.toCharArray()) {
-            letters.add(c);
+        boolean[] seen = new boolean[26];
+        int count = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            int index = sentence.charAt(i) - 'a';
+            if (!seen[index]) {
+                seen[index] = true;
+                count++;
+                if (count == 26) return true; // tüm harfler görüldü
+            }
         }
 
-        return letters.size() == 26;
+        return false;
     }
 }
