5d9adf0e5024c5ab51fcf04e3ae38d47ffd72e91
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2025-10-21 23:36:04 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (36.21%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -1,9 +1,6 @@
 class Solution {
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        // rec = [x1, y1, x2, y2]
-        // Pozitif alanlı kesişim için sıkı (<) karşılaştırmalar gerekir (kenardan/köşeden temas false)
-        boolean xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
-        boolean yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
-        return xOverlap && yOverlap;
+    public boolean isRectangleOverlap(int[] a, int[] b) {
+        // Direkt karşılaştırma, hiçbir ek değişken yok
+        return a[0] < b[2] && b[0] < a[2] && a[1] < b[3] && b[1] < a[3];
     }
 }
