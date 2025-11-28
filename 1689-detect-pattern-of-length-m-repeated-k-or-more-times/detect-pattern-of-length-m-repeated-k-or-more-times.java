a5f4207154e7a18991701cdccb160d6f5a1e174e
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2025-11-27 23:56:50 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (31.90%) - LeetSync

--- 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
@@ -3,18 +3,15 @@ class Solution {
         int n = arr.length;
         int count = 0;
         
-        // i + m < n olduğu sürece döngüye devam et (Array sınırını aşmamak için)
         for (int i = 0; i + m < n; i++) {
             
             if (arr[i] != arr[i + m]) {
-                // Eşleşme bozuldu, sayacı sıfırla
                 count = 0;
             } else {
-                // Eşleşme var (i. eleman ile m adım sonrası aynı)
                 count++;
             }
             
-            // Eğer yeterli sayıda ardışık eşleşme bulduysak (k-1 tane m uzunluğunda blok)
+    
             if (count == (k - 1) * m) {
                 return true;
             }
