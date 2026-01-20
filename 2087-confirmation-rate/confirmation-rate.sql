501e8aae6be07bb32c44fb2773d3392e45ff5d35
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2026-01-19 23:52:43 -0500

    Time: 664 ms (30.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,11 @@
+SELECT 
+    s.user_id, 
+    ROUND(IFNULL(AVG(c.action = 'confirmed'), 0), 2) AS confirmation_rate
+FROM 
+    Signups s
+LEFT JOIN 
+    Confirmations c 
+ON 
+    s.user_id = c.user_id
+GROUP BY 
+    s.user_id;
