81c878efee3a0c8061cb007c90c313e62b659b90
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2026-03-22 23:53:24 -0400

    Time: 2 ms (51.41%) | Memory: 43.4 MB (93.19%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,29 +1,35 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder res = new StringBuilder();
-        int i = a.length() - 1; // a string'inin sonu
-        int j = b.length() - 1; // b string'inin sonu
-        int carry = 0;          // elde tutulan değer
+        // Sonucu toplamak için hızlı bir string oluşturucu
+        StringBuilder sb = new StringBuilder();
+        
+        // String'lerin en sonundan (sağdaki basamaktan) başlıyoruz
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0; // Elde tutulan değer
 
-        // Stringler bitene kadar veya elde (carry) kalmayana kadar devam et
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int sum = carry; // Önceki basamaktan gelen eldeyi ekle
+        // Stringler bitene kadar veya elde (carry) kalmayana kadar döngü çalışır
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry; // Önceki basamaktan gelen eldeyi ekleyerek başla
             
+            // Eğer a string'inde hala basamak varsa ekle
             if (i >= 0) {
-                sum += a.charAt(i--) - '0'; // Char'ı int'e çevir ve ekle
+                sum += a.charAt(i--) - '0'; // '1' - '0' işlemi char'ı int (1) yapar
             }
+            
+            // Eğer b string'inde hala basamak varsa ekle
             if (j >= 0) {
-                sum += b.charAt(j--) - '0'; // Char'ı int'e çevir ve ekle
+                sum += b.charAt(j--) - '0';
             }
             
-            // Mevcut basamağa yazılacak değer (0 veya 1)
-            res.append(sum % 2);
+            // Toplamın 2'ye bölümünden kalan (0 veya 1) o basamağın değeridir
+            sb.append(sum % 2);
             
-            // Bir sonraki basamağa aktarılacak elde (0 veya 1)
+            // Toplamın 2'ye bölümü (0 veya 1) sonraki basamağa aktarılacak eldedir
             carry = sum / 2;
         }
         
-        // Tersten eklediğimiz için string'i çevirip döndür
-        return res.reverse().toString();
+        // Basamakları sondan eklediğimiz için string'i ters çevirip döndürüyoruz
+        return sb.reverse().toString();
     }
 }
