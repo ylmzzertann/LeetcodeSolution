7b4f2ee0a9d562593c4d85502e9e62cdc4f6922b
Ertan Yilmaz <ertanylmz2012@hotmail.com>
2026-07-19 23:57:57 -0400

    Time: 23 ms (35.37%) | Memory: 13.7 MB (15.67%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [-1]  # Base case to handle valid substrings starting at index 0
+        max_len = 0
+        
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    # The stack is empty; this ')' is an unmatched barrier.
+                    # Push its index as the new base anchor.
+                    stack.append(i)
+                else:
+                    # Valid substring found! Calculate length using the current index
+                    # minus the index of the last unmatched element remaining in the stack.
+                    max_len = max(max_len, i - stack[-1])
+                    
+        return max_len
